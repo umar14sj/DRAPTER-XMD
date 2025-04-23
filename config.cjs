f314425a5e03ee8a1dea49ea54fcbe862221a2d9
umar14sj <Seebuumar14@gmail.com>
2025-04-23 18:07:00 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "DRAPTER-XMD~p5QVHAJB#zWQrFOWv3lo3eE2qbUzFe9ZRkBcqqSpM_UESj6yer1U",
+  SESSION_ID: process.env.SESSION_ID || "DRAPTER-XMD~t4R1yLIb#aj1JciKD73NKnj_nfwZcms6Wz73KDlHhnQkGUaAIWX0",
   PREFIX: process.env.PREFIX || '*',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
